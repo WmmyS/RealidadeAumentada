b7159fda0a8477300bf69b6eced2ac0628a0648f
WmmyS <wesleymarcosm@gmail.com>
2022-03-19 16:36:35 -0300

    feat: Adição de detecção de objetos a partir de uma referência de imagem.

--- ReconhecimentoObjetos/main.py
+++ ReconhecimentoObjetos/main.py
@@ -1,26 +1,50 @@
-import cv2 
+import cv2
 from matplotlib import pyplot as plt
-  
-img = cv2.imread("image/image.jpg")
-  
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
-  
-stop_data = cv2.CascadeClassifier(cv2.data.haarcascades +'stop_data')
+import videoCapture as video
 
-minSize =(20, 20)  
+# Como parâmetro também pode ser o diretório do video
+captura = cv2.VideoCapture(0)
 
-found = stop_data.detectMultiScale(img_gray, minSize)
-amount_found = len(found)
+if __name__ == "__main__":
   
-if amount_found != 0: 
+  # Imagem para ser detectada
+  toDetect = cv2.imread("image/toDetect.jpeg")
+  
+  # Selecionar zona de interesse
+  box = cv2.selectROI("select roi", toDetect, fromCenter=False, showCrosshair=False)
+  print(box)
 
-    for (x, y, width, height) in found: 
-        
-      cv2.rectangle(img_rgb, (x, y),  
-                      (x + height, y + width),  
-                      (0, 255, 0), 5) 
-          
-plt.subplot(1, 1, 1) 
-plt.imshow(img_rgb) 
-plt.show() 
+  # Tracker responsável por detectar os objetos
+  tracker = cv2.TrackerCSRT_create()
+  
+  tracker.init(toDetect, box)
+
+  while captura.isOpened():
+    
+    ret, frame = captura.read()
+    
+    if not ret:
+      break
+    
+    ok, box  = tracker.update(frame)
+    
+    # Criação do retângulo mostrado
+    if ok:
+      print('ok')
+      pt1 = (box[0], box[1])
+      pt2 = ((box[0]+box[2]), (box[1] + box[3]))
+      cv2.rectangle(frame, pt1, pt2, (255,0,0),2,1)
+      
+    else:
+      print('FALHA')
+    
+    # Mostrar o video  
+    cv2.imshow('Video', frame)
+    
+    if cv2.waitKey(1) == ord('q'):
+      break 
+    
+  # Retirar vestígios na memória  
+  captura.release()
+  cv2.destroyAllWindows()
+      
--- ReconhecimentoObjetos/videoCapture.py
+++ ReconhecimentoObjetos/videoCapture.py
@@ -2,21 +2,22 @@ from ast import While
 import cv2
 
 # Capturar a imagem de dispositivo
-video = cv2.VideoCapture(0);
-
-while True:
-    conectado, frame = video.read()
+def capturarVideo():
+    video = cv2.VideoCapture(0);
     
-    # Exibir video capturado
-    cv2.imshow('Video', frame)
-    
-    # Aguardar a digitação da tecla 'Q'
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# Retirar vestígios na memória
-video.release()
-cv2.destroyAllWindows()
+    while True:
+        conectado, frame = video.read()
+        
+        # Exibir video capturado
+        cv2.imshow('Video', frame)
+        
+        # Aguardar a digitação da tecla 'Q'
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+    # Retirar vestígios na memória
+    video.release()
+    cv2.destroyAllWindows()
 
         
     
